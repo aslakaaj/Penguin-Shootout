fa2d50567bb622cd9c109c4d448c04a752ee7b39
Aslak Aarflot Jønsson <60319109+aslakaaj@users.noreply.github.com>
2020-12-31 14:56:41 +0100

    Update Gun.cs

--- Gun.cs
+++ Gun.cs
@@ -1,10 +1,12 @@
 using UnityEngine;
 
+//This script makes it easier to make new guns
 [CreateAssetMenu(fileName = "New Gun", menuName = "Weapons/Gun")]
 public class Gun : ScriptableObject
 {
     public int damage;
     public int range;
 
+    //The graphics for your gun
     public GameObject gunPrefab;
 }
