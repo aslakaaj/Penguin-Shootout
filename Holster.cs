1fc3d45fed8b76cfcb05337f30acd760946904a3
Aslak Aarflot Jønsson <60319109+aslakaaj@users.noreply.github.com>
2021-02-14 17:31:36 +0100

    Update Holster.cs

--- Holster.cs
+++ Holster.cs
@@ -1,17 +1,30 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Holster : MonoBehaviour
 {
-    //In the inspector you can implement a GUN object (Wich is what were made in the GUN script)
-    [SerializeField]
-    private Gun weaponToSpawn;
+    [HideInInspector]
+    public Gun equippedGun;
+    public Gun weaponToStartWith;
+    
+    private GameObject weapon;
+    public bool isEquipped = false;
 
-    //When this script awakes, the selected gun will Instanstiate in the game
     void Awake()
     {
-        GameObject weapon = Instantiate(weaponToSpawn.gunPrefab, transform.position, transform.rotation) as GameObject;
+        if (equippedGun != null)
+        {
+            equippedGun = null;
+        }
+
+        EquipGun(weaponToStartWith);
+
+        weapon = Instantiate(equippedGun.gunPrefab, transform.position, transform.rotation) as GameObject;
         weapon.transform.parent = transform;
+        isEquipped = true;
+    }
+
+    public void EquipGun(Gun _GunToEquip)
+    {
+        equippedGun = _GunToEquip;
     }
 }
