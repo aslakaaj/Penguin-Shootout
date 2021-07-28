ff1bcb4903f7488d565c247d57d7002d9d570eff
Aslak Aarflot Jønsson <60319109+aslakaaj@users.noreply.github.com>
2021-07-28 14:33:10 +0200

    Update Projectile.cs

--- Projectile.cs
+++ Projectile.cs
@@ -2,7 +2,24 @@ using UnityEngine;
 
 public class Projectile : MonoBehaviour
 {
+    [SerializeField]
     private float shotSpeed;
+    private float controlSpeed;
+    [SerializeField]
+    private float dSpeed = 1;
+
+    private Enemy enemy;
+    private Holster holster;
+    private Vector3 pushForce;
+    private Vector3 bodyVector;
+
+    private Rigidbody rb;
+
+    private void Awake()
+    {
+        rb = gameObject.GetComponent<Rigidbody>();
+        holster = FindObjectOfType<Holster>();
+    }
 
     public void newSpeed(float _newSpeed)
     {
@@ -11,6 +28,32 @@ public class Projectile : MonoBehaviour
 
     private void Update()
     {
-        transform.Translate(Vector3.forward * Time.deltaTime * shotSpeed);
+        Vector3 test = Vector3.forward * Time.deltaTime * shotSpeed;
+        transform.Translate(test);
+
+        if (test.z >= holster.equippedGun.range)
+        {
+            rb.useGravity = true;
+        }
+
+        shotSpeed += dSpeed;
+
+    }
+
+    private void OnTriggerEnter(Collider collision)
+    {
+        if (collision.gameObject.tag == "Enemy")
+        {
+            enemy = collision.gameObject.GetComponent<Enemy>();
+            enemy.applyDamage(holster.equippedGun.damage);
+            enemy.AddPushBackForce(pushForce * 500f, bodyVector);
+            Destroy(gameObject);
+        }
+    }
+
+    public void newKnockbackForce(Vector3 _pushForce, Vector3 _bodyVector)
+    {
+        pushForce = _pushForce;
+        bodyVector = _bodyVector;
     }
 }
